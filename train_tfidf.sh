e9c7ac96839048f5be84f4a989975ab4f697d846
Falkie <35712723+michaelgfalk@users.noreply.github.com>
2020-04-07 18:08:51 +1000

    fixed iterators

--- train_tfidf.sh
+++ train_tfidf.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --job-name=FastTextLitReview
+#SBATCH --job-name=TopicModelTraining
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=mgf@kent.ac.uk
 #SBATCH --output=/home/mgf/slurm/logs/%j.out
--- train_topic_models.py
+++ train_topic_models.py
@@ -7,18 +7,18 @@ corpus = JSTORCorpus(data_dir='data/ocr', meta_dir='data/metadata')
 corpus.save('data/whole-corpus.p')
 
 # Create dictionary
-dct = Dictionary(corpus.iter_lower)
+dct = Dictionary(corpus.iter_lower())
 dct.filter_extremes(no_below=5)
 dct.filter_n_most_frequent(remove_n=50)
 dct.save('models/corpus-lower-dct')
 
 # Train tf-idf model
-bow_corpus = (dct.doc2bow(text) for text in corpus.iter_lower)
+bow_corpus = (dct.doc2bow(text) for text in corpus.iter_lower())
 tfidf_model = TfidfModel(bow_corpus)
 tfidf_model.save('models/corpus-lower-tfidf')
 
 # Train lda model
-bow_corpus = (dct.doc2bow(text) for text in corpus.iter_lower)
+bow_corpus = (dct.doc2bow(text) for text in corpus.iter_lower())
 lda_model = LdaModel(bow_corpus, num_topics=150)
 lda_model.save('models/corpus-lower-lda')
 
