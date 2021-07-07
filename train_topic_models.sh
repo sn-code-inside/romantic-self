5f63ba0241ced4cace92ce514d884ec7ab8e9a37
Falkie <35712723+michaelgfalk@users.noreply.github.com>
2021-07-07 13:54:04 +0100

    tidying up

--- train_topic_models.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
-#SBATCH --job-name=LitReviewLDAandTfIdf
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=mgf@kent.ac.uk
-#SBATCH --output=/home/mgf/slurm/logs/%j.out
-python train_topic_models.py
